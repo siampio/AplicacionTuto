0ba98b7928f60a50488b517a98950f11856d48c5
Duvan Lopez <stevenbarcalop@gmail.com>
2017-09-22 21:55:03 -0500

    LoginDesign

--- app/src/main/java/proyectutorias/apptutoriasusbbog/Fragments/loginFragment.java
+++ app/src/main/java/proyectutorias/apptutoriasusbbog/Fragments/loginFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import proyectutorias.apptutoriasusbbog.R;
 
@@ -14,10 +16,23 @@ import proyectutorias.apptutoriasusbbog.R;
  */
 
 public class loginFragment extends Fragment{
+    Switch perfil;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        perfil = (Switch)rootView.findViewById(R.id.switchPerfil);
+        perfil.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    perfil.setText("Tutor");
+                }else {
+                    perfil.setText("Estudiante");
+                }
+            }
+        });
+
         return rootView;
     }
 }
